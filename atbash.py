fe47a9fc149a345443949f7a4d25c5d588723e64
Vinicius Libano <34941776+ViniciusLibano@users.noreply.github.com>
2023-10-23 22:27:21 -0300

    atbash decode

--- atbash.py
+++ atbash.py
@@ -1,17 +1,22 @@
 alphabet = 'abcdefghijklmnopqrstuvwxyz'
 
+invert = ''
+i = -1
+for l in alphabet:
+    invert += alphabet[i]
+    i = i - 1
+
 def encode(text: str):
     clean = ''.join(char for char in text if char.isalnum()).lower()
     for num in '1234567890':
         clean = clean.replace(num, '')
-    iAlpha = ''
-    i = -1
-    for l in alphabet:
-        iAlpha += alphabet[i]
-        i = i - 1
     result = ''
     for letter in text:
-        result += iAlpha[alphabet.index(letter)]
+        result += invert[alphabet.index(letter)]
     return result
 
-print(encode('abc'))
+def decode(text: str):
+    result = ''
+    for letter in text:
+        result += alphabet[invert.index(letter)]
+    return result
